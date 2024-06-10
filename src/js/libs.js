4521f8e30afbb07aa0c7fb1a366aa782005af7ec
bokovushka <andriibokovyi@gmail.com>
2024-06-10 11:24:36 +0300

    update

--- src/js/libs.js
+++ src/js/libs.js
@@ -3,6 +3,7 @@ import "./modules/libs/with-lib-bootstrap.js";
 import "./modules/libs/with-lib-swiper.js";
 import "./modules/libs/with-lib-select2.js";
 import "./modules/libs/with-lib-plyr.js";
+import "./modules/libs/with-lib-fancybox.js";
 // import "./modules/libs/with-lib-plyr&swiper&fancybox.js";
 // import "./modules/libs/with-lib-readmore.js";
 
---
+++ src/js/modules/libs/with-lib-fancybox.js
@@ -0,0 +1,33 @@
+import { Fancybox } from "@fancyapps/ui";
+
+//? clinic-gallery
+Fancybox.bind('[data-fancybox="clinic-gallery"]', {
+	Toolbar: {
+		display: {
+			left: [],
+			right: ["close"],
+		},
+	},
+	Carousel: {
+		Navigation: {
+			nextTpl: `<svg><use xlink:href="img/icons/icons.svg#i-arrow-right"></use></svg>`,
+			prevTpl: `<svg><use xlink:href="img/icons/icons.svg#i-arrow-right"></use></svg>`,
+		},
+	},
+	contentClick: "iterateZoom",
+	Images: {
+		Panzoom: {
+			maxScale: 3,
+		},
+	},
+	Thumbs: false,
+	caption: (fancybox, slide) => {
+		const caption = slide.caption || "";
+
+		return `${slide.index + 1} / ${fancybox.carousel?.slides.length
+			} <br /> ${caption}`;
+	},
+});
+
+
+
