66be18d3f0afb869f362c2042c86a728741d3401
bokovushka <andriibokovyi@gmail.com>
2024-06-11 16:18:53 +0300

    update

--- src/js/libs.js
+++ src/js/libs.js
@@ -1,11 +1,10 @@
 //libs
 import "./modules/libs/with-lib-bootstrap.js";
+import "./modules/libs/with-lib-swiper.js";
+import "./modules/libs/with-lib-select2.js";
+import "./modules/libs/with-lib-plyr.js";
+import "./modules/libs/with-lib-fancybox.js";
 import "./modules/libs/with-lib-photo-sphere-viewer.js";
-// import "./modules/libs/with-lib-swiper.js";
-// import "./modules/libs/with-lib-select2.js";
-// import "./modules/libs/with-lib-plyr.js";
-// import "./modules/libs/with-lib-fancybox.js";
-
 
 
 // import "./modules/libs/with-lib-plyr&swiper&fancybox.js";
