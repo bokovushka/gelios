dad8d3198c25dde49092f648a8a48c0f897d89a0
bokovushka <andriibokovyi@gmail.com>
2024-06-17 16:35:39 +0300

    update

--- src/js/modules/js.js
+++ src/js/modules/js.js
@@ -1,15 +1,15 @@
-function adjustHeight() {
-	if (window.innerWidth < 1024) {
-		const mainContent = document.querySelector('.header-main--mob');
-		mainContent.style.height = `calc(${window.innerHeight}px - 124px - env(safe-area-inset-top))`;
-	} else {
-		// Скидаємо висоту для екранів більше 1024px
-		document.querySelector('.header-main--mob').style.height = 'auto';
-	}
-}
+// function adjustHeight() {
+// 	if (window.innerWidth < 1024) {
+// 		const mainContent = document.querySelector('.header-main--mob');
+// 		mainContent.style.height = `calc(${window.innerHeight}px - 124px - env(safe-area-inset-top))`;
+// 	} else {
+// 		// Скидаємо висоту для екранів більше 1024px
+// 		document.querySelector('.header-main--mob').style.height = 'auto';
+// 	}
+// }
 
-window.addEventListener('resize', adjustHeight);
-window.addEventListener('orientationchange', adjustHeight);
+// window.addEventListener('resize', adjustHeight);
+// window.addEventListener('orientationchange', adjustHeight);
 
-// Викликати функцію при завантаженні сторінки
-adjustHeight();
+// // Викликати функцію при завантаженні сторінки
+// adjustHeight();
