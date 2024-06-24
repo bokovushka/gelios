67b850248b9d84b9e1cf529a754e1c7035a02a0b
bokovushka <andriibokovyi@gmail.com>
2024-06-24 11:36:04 +0300

    update

--- src/js/libs.js
+++ src/js/libs.js
@@ -4,4 +4,5 @@ import "./modules/libs/with-lib-swiper.js";
 import "./modules/libs/with-lib-select2.js";
 import "./modules/libs/with-lib-plyr.js";
 import "./modules/libs/with-lib-fancybox.js";
+import "./modules/libs/with-lib-readmore.js";
 import "./modules/libs/with-lib-photo-sphere-viewer.js";
--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,6 @@ import "./modules/show-menu.js";
 import "./modules/multi-level-push-menu.js";
 import "./modules/mobile-menu.js";
 import "./modules/height-vh-root.js";
+import "./modules/counter-textarea.js";
+import './modules/scroll-class.js'
 // import "./modules/js.js";
